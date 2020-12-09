e56b5115dbbb12f930852774abcd4db5c1820d5f
constantinpri <pricochi.constantin@gmail.com>
2020-12-09 08:30:42 -0800

    added new files

--- cypress/integration/myTests/get_api_test.js
+++ cypress/integration/myTests/get_api_test.js
@@ -2,8 +2,7 @@
 describe('Get API for Rest', ()=>{
 
     it('GET Api Test for Flask - MotorBike', ()=>{
-
-        cy.request('https://flask-rest-api-demo.herokuapp.com/product/motorbike').then((res) => {
+        cy.request('GET', 'https://flask-rest-api-demo.herokuapp.com/product/motorbike').then((res) => {
             expect(res.status).equal(200)
             expect(res.body.product[0]).has.property('price', 599.99)
             expect(res.body.product[0]).has.property('product', 'motorbike')
@@ -11,13 +10,21 @@ describe('Get API for Rest', ()=>{
     })
 
     it('GET Api Test for Flask - Users', ()=>{
-
-        cy.request('https://flask-rest-api-demo.herokuapp.com/users').then((res) => {
+        cy.request('GET','https://flask-rest-api-demo.herokuapp.com/users').then((res) => {
             expect(res.status).equal(200)
             expect(res.body.users[0]).has.property('username', 'test_1')
             expect(res.body.users[1]).has.property('id', 2)
             expect(res.body.users).has.length(5)
             expect(res.body.users[0]).not.have.property('price')
+        }) 
+    })
+
+    it('GET Api Test for Request Res API - Users', ()=>{
+        cy.request('GET','https://reqres.in/api/users?page=2').then((res) => {
+            expect(res.status).equal(200)
+            expect(res.body.data[0]).has.property('first_name', 'Michael')
+            expect(res.body.data[1]).has.property('email', 'lindsay.ferguson@reqres.in')
+            expect(res.body.data).has.length(6)
         }) 
     })
 
